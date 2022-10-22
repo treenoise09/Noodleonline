e23fc0dec274ba7397724df9ac2b292924e893fe
wasa wasab <treenoise09@gmail.com>
2022-10-22 19:27:59 +0700

    navbar create

---
+++ src/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import './Header.css'
+import Logo from './img/Onlinenoodle-logos_transparent.png'
+function Header() {
+    return(
+        <div className="header">
+            <img className="header_logo" src={Logo}/>
+            <div className="header_search">
+                <input className="header_searchInput" type='text'/>
+
+            </div>
+            <div className="header_nav">
+            <div className="header_option">
+                    
+                    </div>
+                    <div className="header_option">
+                    
+                    </div>
+                    <div className="header_option">
+                    
+                    </div>
+                    <div className="header_option">
+                    
+                    </div>
+            </div>
+        </div>
+    )
+    
+}
+
+export default Header
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAxvgDk1lNDQWOlprCibt7mkGCPwKsw5_Y",
+    authDomain: "onlinenoodle-c7b8b.firebaseapp.com",
+    databaseURL: "https://onlinenoodle-c7b8b-default-rtdb.firebaseio.com",
+    projectId: "onlinenoodle-c7b8b",
+    storageBucket: "onlinenoodle-c7b8b.appspot.com",
+    messagingSenderId: "547348314049",
+    appId: "1:547348314049:web:141d06dba160e2f42ae140",
+    measurementId: "G-HMN04ZTFDW"
+  };
