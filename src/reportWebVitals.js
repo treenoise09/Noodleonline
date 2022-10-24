6b31ff2e23b400252df8b3723aaa8dc72606bf63
wasa wasab <treenoise09@gmail.com>
2022-10-24 08:47:45 +0700

    add searchbutton

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,13 +1,13 @@
+import { getLCP,getFID,getFCP,getCLS,getTTFB } from 'web-vitals';
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
       getLCP(onPerfEntry);
       getTTFB(onPerfEntry);
-    });
-  }
+    };
 };
+
 
 export default reportWebVitals;
